fcda50e1fc0afda568c0be763a2f7c98284ccbc9
Vicky Kumar <kumarvky472@gmail.com>
2024-07-30 01:50:03 +0530

    new changes

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,48 @@
 import express from "express";
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("Connected to MongoDB");
+})
+.catch((err) => {
+    console.log(err);
+})
+
 
 const app=express();
 
 const PORT=process.env.PORT || 3000
 
+app.use(express.json());
+app.use(cookieParser())
+
+
 app.get("/",(req,res) => {
     res.send("Hello World")
 })
 
+// import routes
+import authRoutes from './routes/auth.routes.js'
+import messageRoute from './routes/message.routes.js'
+app.use('/api/auth',authRoutes)
+app.use('/api/message',authRoutes)
+
 app.listen(PORT,() =>{
     console.log("Server is running on port " + PORT)
 })
+
+// error handler
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message || "Internal Server Error"
+  
+    return res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    })
+  })
